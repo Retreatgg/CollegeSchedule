c4a61b9f660534b123d0c4f429b9a2dd92ee32ec
Ashimov Argen <retreatpm@icloud.com>
2024-09-24 22:03:31 +0300

    Fix error with data sql

--- src/main/resources/db/changelog/initial/010_create_some_user.sql
+++ src/main/resources/db/changelog/initial/010_create_some_user.sql
@@ -5,7 +5,7 @@ values ('TEACHER'),
 insert into users(name, surname, patronymic, email, password, enabled, token)
 VALUES('Канат', 'Cыдыков', 'ХЗ', 'kanat@gail.com', '$2a$12$WB2YUbFcCN0tm44SBcKUjua9yiFBsfB3vW02IjuwzY7HGtlQIKzy2', true, '123456789');
 
-insert into user_roles(user_id, role_id) VALUES (2, 2);
+insert into user_roles(user_id, role_id) VALUES (1, 2);
 
 insert into audiences_type(name)
 values ('Компьютерная'),
