5f97b96a47b46cc44e581f5b7336bd8bd849aaef
Argen Ashimov <argenashimov@MacBook-Air-Argen.local>
2024-10-13 20:38:55 +0600

    Fix sintax error

--- src/main/resources/db/changelog/initial/010_create_some_user.sql
+++ src/main/resources/db/changelog/initial/010_create_some_user.sql
@@ -6,8 +6,8 @@ insert into users(name, surname, patronymic, email, password, enabled, token)
 VALUES('Канат', 'Cыдыков', 'Кадырбекович', 'kanat@gmail.com', '$2a$12$WB2YUbFcCN0tm44SBcKUjua9yiFBsfB3vW02IjuwzY7HGtlQIKzy2', true, '123456789'),
 ('Арген', 'Ашимов', 'Чынгызбекович', 'retreatpm@icloud.com', '$2a$12$WB2YUbFcCN0tm44SBcKUjua9yiFBsfB3vW02IjuwzY7HGtlQIKzy2', true, '123456789'),
 ('', '', '', 'maksatkanybekov2004@gmail.com', '$2a$12$WB2YUbFcCN0tm44SBcKUjua9yiFBsfB3vW02IjuwzY7HGtlQIKzy2', true, null),
-      ('', '', '', 'comtehno.kg@gmail.com', '$2a$12$WB2YUbFcCN0tm44SBcKUjua9yiFBsfB3vW02IjuwzY7HGtlQIKzy2', true, null)
-    ('', '', '', 'asanovkurmanbek342@gmail.com', '$2a$12$WB2YUbFcCN0tm44SBcKUjua9yiFBsfB3vW02IjuwzY7HGtlQIKzy2', true, null)
+      ('', '', '', 'comtehno.kg@gmail.com', '$2a$12$WB2YUbFcCN0tm44SBcKUjua9yiFBsfB3vW02IjuwzY7HGtlQIKzy2', true, null),
+    ('', '', '', 'asanovkurmanbek342@gmail.com', '$2a$12$WB2YUbFcCN0tm44SBcKUjua9yiFBsfB3vW02IjuwzY7HGtlQIKzy2', true, null),
 ('', '', '', 'malabakievramzan@gmail.com', '$2a$12$WB2YUbFcCN0tm44SBcKUjua9yiFBsfB3vW02IjuwzY7HGtlQIKzy2', true, null),
 ('', '', '', 'malabakievramzan@gmail.com', '$2a$12$WB2YUbFcCN0tm44SBcKUjua9yiFBsfB3vW02IjuwzY7HGtlQIKzy2', true, null);
 
