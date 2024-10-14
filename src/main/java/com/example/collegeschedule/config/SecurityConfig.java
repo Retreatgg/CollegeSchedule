23939cff919a150d915fcafd7b42dd9872873281
argenashimov <retreatpm@icloud.com>
2024-10-14 17:21:35 +0600

    add address

--- src/main/java/com/example/collegeschedule/config/SecurityConfig.java
+++ src/main/java/com/example/collegeschedule/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://172.20.10.2:3000/", "https://schedule.makalabox.com/"));
+        configuration.setAllowedOrigins(List.of("http://169.254.115.56:3000/", "https://schedule.makalabox.com/", "http://localhost:3000/"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
         configuration.setAllowCredentials(true);
